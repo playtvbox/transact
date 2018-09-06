040b83f9a3816a250f308be2c2e31836067eebe4
playtvbox <43045856+playtvbox@users.noreply.github.com>
2018-09-06 13:51:04 -0500

    Add files via upload

---
+++ channellists.py
@@ -0,0 +1,15 @@
+﻿channellister=[
+        ("اخبار بی بی سی فارسی", "playlist/PLmdEvtplre63hcXYBbS5AE1iFkoEH4aDO", 'https://lh3.googleusercontent.com/a-/ACSszfHgmP5z1wQy7_Kok9FUBLIYQ1uUEmb9fzqb4Q=s176-c-k-c0x00ffffff-no-rj-mo'),
+		("اخبار صدای آمریکا", "user/PNNVideo", 'https://yt3.ggpht.com/-OJW2fX0DTjU/AAAAAAAAAAI/AAAAAAAAAAA/4JSco0sj7-Y/s288-mo-c-c0xffffffff-rj-k-no/photo.jpg'),
+		("اخبار منوتو", "playlist/PLXiFD28PjqaIP1QRllu1FLDHbV5QQ8pWO", 'https://lh3.googleusercontent.com/a-/ACSszfEZqQF2OTyMHBAfYzz3EJVgVx0mXliVM_He=s176-c-k-c0x00ffffff-no-rj-mo'),
+		("اخبار یورونیوز", "user/euronewspe", 'https://yt3.ggpht.com/-XEigx4JLZBo/AAAAAAAAAAI/AAAAAAAAAAA/J0a413_W8CU/s288-mo-c-c0xffffffff-rj-k-no/photo.jpg'),
+		("اخبار صدا و سیما","channel/UCTJmxoAtMRpRZ6QUx0ufOjQ",'https://yt3.ggpht.com/-1WlMZDTQzAw/AAAAAAAAAAI/AAAAAAAAAAA/HXcIsnQnd1Y/s288-mo-c-c0xffffffff-rj-k-no/photo.jpg'),
+        ("اخبار ورزشی","playlist/PLDNLM_vuSXhSfHpGau2QguODLa3eLVhTZ",'https://i.ytimg.com/vi/Df1NbD-yRvI/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLBsUNrSq7x60GfuL1OfmfRzOppQCA'),
+	    ("World News","playlist/PLr1-FC1l_JLFcq9r9Y3uFLkH8G37WmMRQ",'https://lh3.googleusercontent.com/wFzIMQkD7T4GelAT7xOs0gTZ9tiUg_dkuEfNCWeHj6UhcGf-lFLcmOM5xetp2IAEL3e9HW4Vjj15pb3O=s176-c-k-c0x00ffffff-no-rj-mo'),
+        ("National News","playlist/PLNjtpXOAJhQLmUEyuWw4hW_6gX8JMJUof",'https://lh3.googleusercontent.com/a-/ACSszfGF1uYUIHJY68eCr3fCaquacUYRgJnM_KFnHA=s176-c-k-c0x00ffffff-no-rj-mo'),
+        ("Cnn ","playlist/PL6XRrncXkMaUoSMd-1D5uIt7uZ0nWxkMy",'https://lh3.googleusercontent.com/a-/ACSszfGIoSEbpbPNi85rjsFeVVsPimLGcfJ2bH8_2g=s176-c-k-c0x00ffffff-no-rj-mo'),
+        ("Nbc News","playlist/PL0tDb4jw6kPxZ7ZJtMk6gqiQIU-nMeaVS",'https://lh3.googleusercontent.com/a-/ACSszfF6fLlzAOd2WLWBmIzIu8Jc-ZutbeEL-Vd2qQ=s176-c-k-c0x00ffffff-no-rj-mo'),
+        ("ABC","playlist/PL5E6638EB9ECF2329",'https://lh3.googleusercontent.com/a-/ACSszfGOVyE9CVKBg7L7102Uxrbvy03OwLdlwbmAhA=s176-c-k-c0x00ffffff-no-rj-mo'),
+        ("Fox News","playlist/PLlTLHnxSVuIyeEZPBIQF_krewJkY2JSwi",'https://lh3.googleusercontent.com/a-/ACSszfG57RAbrL7Kbrwj2SZ8sK_kgMd2RjSg9Fescw=s176-c-k-c0x00ffffff-no-rj-mo'),
+        ("Cbs News","playlist/PLEb3ThbkPrFabMf542Iic5Hxw2xlrgusX",'https://lh3.googleusercontent.com/a-/ACSszfHemtHbPUQkIjBAw4iS4KcjRkU2VoWFUu2wrQ=s176-c-k-c0x00ffffff-no-rj-mo')
+]
